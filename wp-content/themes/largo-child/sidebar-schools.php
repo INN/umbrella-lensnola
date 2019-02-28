22b008b55865bfa7badd3cf0f8d593d75f63f39d
Ben Keith <ben@benlk.com>
2019-02-27 20:26:45 -0500

    Modernize sidebar-schools

--- wp-content/themes/largo-child/sidebar-schools.php
+++ wp-content/themes/largo-child/sidebar-schools.php
@@ -1,12 +1,24 @@
 <?php
 /**
  * The Sidebar containing the schools widget area.
+ *
+ * @since Largo 0.6.1
  */
+
+$span_class = largo_sidebar_span_class();
+do_action('largo_before_sidebar');
 ?>
-<div class="widget-area" role="complementary">
-	<?php
-		if ( ! dynamic_sidebar( 'sidebar-schools' ) ) :
-			
-		endif;
-	?>
-</div><!-- #main .widget-area -->
+<aside id="sidebar" class="<?php echo $span_class; ?> nocontent">
+	<?php do_action('largo_before_sidebar_content'); ?>
+	<div class="widget-area" role="complementary">
+		<?php
+			do_action('largo_before_sidebar_widgets');
+
+			dynamic_sidebar( 'sidebar-schools' );
+
+			do_action('largo_after_sidebar_widgets');
+		?>
+	</div><!-- #main .widget-area -->
+	<?php do_action('largo_after_sidebar_content'); ?>
+</aside>
+<?php do_action('largo_after_sidebar');
