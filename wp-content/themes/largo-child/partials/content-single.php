caddd9d5623cd8f826f5a656d2dd3451e272b1eb
Ben Keith <ben@benlk.com>
2019-02-28 17:48:16 -0500

    Remove more differences between partials/content-single in this theme and Largo

--- wp-content/themes/largo-child/partials/content-single.php
+++ wp-content/themes/largo-child/partials/content-single.php
@@ -4,7 +4,9 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?> itemscope itemtype="https://schema.org/Article">
+
+	<?php do_action('largo_before_post_header'); ?>
 
 	<header class="category-background">
 
@@ -15,6 +17,7 @@
 			<h2 class="subtitle"><?php echo $subtitle ?></h2>
 		<?php endif; ?>
 		<h5 class="byline"><?php largo_byline( true, false, get_the_ID() ); ?></h5>
+
 		<?php if ( ! of_get_option( 'single_social_icons' ) == false ) {
 			largo_post_social_links();
 		} ?>
