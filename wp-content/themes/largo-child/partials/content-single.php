3eb4dabad51354e02c17d93de038ae7d5fb74094
Ben Keith <ben@benlk.com>
2019-02-27 19:13:11 -0500

    Reorganize partials/content-single.php to more closely match Largo's current partial of that name

--- wp-content/themes/largo-child/partials/content-single.php
+++ wp-content/themes/largo-child/partials/content-single.php
@@ -2,46 +2,63 @@
 /**
  * The template for displaying content in the single.php template
  */
+$category = largo_child_get_the_category();
+$meta = get_post_meta(get_the_ID(),'_custom_post_type_onomies_relationship',false);
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?>>
+
 	<header class="category-background">
-		<?php $category = largo_child_get_the_category();?>
-        <?php $meta = get_post_meta(get_the_ID(),'_custom_post_type_onomies_relationship',false);?>
-        <h5 class="<?php echo $category->slug;?>">
-            <a href="<?php echo esc_url( largo_child_category_link($category->term_id, $category->slug) );?>"><?php echo $category->name;?></a>
-            <?php if ($meta): ?><a href="#" id="more_coverage_hover" onclick="return false;">Related schools coverage &raquo;</a><?php endif; ?>
-        </h5>
-        <?php if ($meta):?>
-            <?php if (is_array($meta)):
-                foreach ($meta as $value):
-                    $school = get_post($value);
-                    if($school->post_type == 'school'):
-                        $links[] = '<a href="'.get_permalink($school->ID).'">'.get_the_title($school->ID).'</a>';
-                    endif;
-                endforeach;
-            else:
-                $school = get_post($meta);
-                if($school->post_type == 'school'):
-                    $links[] = '<a href="'.get_permalink($school->ID).'">'.get_the_title($school->ID).'</a>';
-                endif;
-            endif;?>
-            <ul class="school_link" style="display: none;">
-                <li><?php echo implode('</li><li>',$links);?></li>
-            </ul>
-        <?php endif;?>
-		<div class="clearfix">&nbsp;</div>
- 		<h1 class="entry-title"><?php the_title(); ?></h1>
- 		<div class="clearfix">
- 			<div class="left">
- 				<div class="byline"><?php largo_byline(true,true,true,true,true); ?><span class="small_comments">&nbsp;&nbsp;&nbsp;&nbsp;<?php largo_child_comments();?></span><?php edit_post_link( __('Edit This Post', 'largo'), ' <div class="edit-link">', '</div>'); ?></div>
- 			</div>
- 			<div class="right">
- 				<?php get_template_part( 'largo-social-single' ); ?>
- 			</div>
- 		</div>
+
+		<h5 class="<?php echo $category->slug;?>">
+			<a href="<?php echo esc_url( largo_child_category_link($category->term_id, $category->slug) );?>"><?php echo $category->name;?></a>
+			<?php if ($meta): ?><a href="#" id="more_coverage_hover" onclick="return false;">Related schools coverage &raquo;</a><?php endif; ?>
+		</h5>
+
+		<?php if ($meta):?>
+			<?php if (is_array($meta)):
+				foreach ($meta as $value):
+					$school = get_post($value);
+					if($school->post_type == 'school'):
+						$links[] = '<a href="'.get_permalink($school->ID).'">'.get_the_title($school->ID).'</a>';
+					endif;
+				endforeach;
+			else:
+				$school = get_post($meta);
+				if($school->post_type == 'school'):
+					$links[] = '<a href="'.get_permalink($school->ID).'">'.get_the_title($school->ID).'</a>';
+				endif;
+			endif;?>
+			<ul class="school_link" style="display: none;">
+				<li><?php echo implode('</li><li>',$links);?></li>
+			</ul>
+		<?php endif;?>
+
+		<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
+		<?php if ( $subtitle = get_post_meta( $post->ID, 'subtitle', true ) ) : ?>
+			<h2 class="subtitle"><?php echo $subtitle ?></h2>
+		<?php endif; ?>
+		<h5 class="byline"><?php largo_byline( true, false, get_the_ID() ); ?></h5>
+		<?php if ( ! of_get_option( 'single_social_icons' ) == false ) {
+			largo_post_social_links();
+		} ?>
+
+<?php largo_post_metadata( $post->ID ); ?>
+
 	</header><!-- / entry header -->
-	<div class="entry-content clearfix">
+
+	<?php
+		do_action('largo_after_post_header');
+
+		largo_hero(null,'span12');
+
+		do_action('largo_after_hero');
+	?>
+
+	<?php get_sidebar(); ?>
+
+	<section class="entry-content clearfix" itemprop="articleBody">
+
 		<?php largo_entry_content( $post ); ?>
 
 		<div class="disclosure-container">
@@ -57,7 +74,8 @@
 				The Lens' <a href="<?php echo home_url('/support-us/supported-by/');?>">donors</a> and <a href="<?php echo home_url('/support-us/supported-by-2/');?>">partners</a> may be mentioned or have a stake in the stories we cover.</a>
 			</div>
 		</div>
-	</div><!-- .entry-content -->
-	<footer class="post-meta bottom-meta">
-	</footer><!-- /.post-meta -->
+	</section><!-- .entry-content -->
+
+	<?php do_action('largo_after_post_content'); ?>
+
 </article><!-- #post-<?php the_ID(); ?> -->
